eb24b64105f5fb7305c31993ebf9e22bfe278fac
Maciej Kazulak <kazulakm@gmail.com>
2017-08-20 17:35:56 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,12 +1,13 @@
 { mkDerivation, aeson, base, containers, data-default, ghcjs-dom
-, lens, reflex-dom, stdenv, text
+, jsaddle, lens, reflex-dom, stdenv, text
 }:
 mkDerivation {
   pname = "reflex-dom-plotly";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base containers data-default ghcjs-dom lens reflex-dom text
+    aeson base containers data-default ghcjs-dom jsaddle lens
+    reflex-dom text
   ];
   license = stdenv.lib.licenses.bsd3;
 }
